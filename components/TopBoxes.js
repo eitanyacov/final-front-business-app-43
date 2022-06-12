069be7b3129cba21f16ebca980f891d6f6aa487c
eitanyacov <eitanyacov@gmail.com>
2022-06-12 04:24:30 +0300

    profile page - number of suppliers - total amount suppliers outcome

--- components/TopBoxes.js
+++ components/TopBoxes.js
@@ -5,27 +5,48 @@ import PeopleAltIcon from '@mui/icons-material/PeopleAlt';
 import MonetizationOnIcon from '@mui/icons-material/MonetizationOn';
 import CalendarMonthIcon from '@mui/icons-material/CalendarMonth';
 import { useRouter } from 'next/router'
-
+import axios from 'axios'
 
 
 const TopBoxes = () => {
   const router = useRouter();
   const [numberOfSuppliers, setNumberofSuppliers] = useState();
+  const [user, setUser] = useState({})
+  const [outcome, setOutcome] = useState()
+
 
   useEffect(()=> {
-    getNumberOfSuppliers();
-  }, [])
+    const res = localStorage.getItem("user")
+    const result = JSON.parse(res)
+    setUser(result)
+    
+}, [])
 
-  const getNumberOfSuppliers = async () => {
-    const result = await fetch("http://localhost:8080/api/user/number-of-suppliers");
-    const data = await result.json();
-    setNumberofSuppliers(data);
-  }
+  useEffect(()=> {
+    const id = user?.id;
+    axios.get(`http://localhost:8080/api/user/number-of-suppliers/${id}`)
+    .then(res => setNumberofSuppliers(res.data))
+    .catch(err => console.log(err))
+  }, [user?.id])
+
+  useEffect(()=> {
+    const id = user?.id;
+    axios.get(`http://localhost:8080/api/user/total-amount/${id}`)
+    .then(res => setOutcome(res.data))
+    .catch(err => console.log(err))
+  }, [user?.id])
+
+  // const getNumberOfSuppliers = async () => {
+    
+  //   const result = await fetch(`http://localhost:8080/api/user/number-of-suppliers/12`);
+  //   const data = await result.json();
+  //   setNumberofSuppliers(data);
+  // }
   console.log("number of suppliers: " + numberOfSuppliers)
   return (
     <div className='flex ml-[80px] md:ml-[200px] justify-around p-1 space-x-2'>
         <TopBox title="SUPPLIERS" amount={numberOfSuppliers} link="See all suppliers" icon={PeopleOutlineIcon} color="success" onClick={()=> router.push('/suppliers')}/>
-        <TopBox title="OUTCOME"  amount="3484$" link="Watch details" icon={PeopleAltIcon}  color="primary" onClick={()=> router.push('/agents')}/>
+        <TopBox title="OUTCOME"  amount={outcome} link="Watch details" icon={PeopleAltIcon}  color="primary" onClick={()=> router.push('/agents')}/>
         <TopBox title="INCOME" amount="15478$" link="View net income" icon={CalendarMonthIcon}  color="secondary" />
         <TopBox title="BALANCE" amount="7240$" link="See details" icon={MonetizationOnIcon}  color="info"/>
     </div>
--- pages/profile/index.js
+++ pages/profile/index.js
@@ -13,12 +13,49 @@ const Profile = () => {
       
       
   }, [])
+
   
   if(!user) router.push('login')
+
   return (
     <div>
         <SideBarPage />
-        <h1 className='ml-[250px]'>Profile Page</h1>
+        <div className='flex ml-[220px] max-w-[1200px] bg-slate-100 min-h-screen p-3 '>
+              <div className='w-[350px] bg-[#f6f3f3] h-[600px] p-3 space-y-5 rounded-lg fixed'>
+                  <div className='flex justify-between items-center'>
+                  <h1 className='text-[#333] text-2xl font-bold'>First Name :</h1>
+                  <h1 className='text-[#333] text-2xl'>{user?.firstName}</h1>
+                  </div>
+                  <div className='flex justify-between items-center'>
+                  <h1 className='text-[#333] text-2xl font-bold'>Last Name :</h1>
+                  <h1 className='text-[#333] text-2xl'>{user?.lastName}</h1>
+                  </div>
+                  <div className='flex justify-between items-center'>
+                  <h1 className='text-[#333] text-2xl font-bold'>Email :</h1>
+                  <h1 className='text-[#333] text-2xl'>{user?.email}</h1>
+                  </div>
+                  <div className='flex justify-between items-center'>
+                  <h1 className='text-[#333] text-2xl font-bold'>Company : </h1>
+                  <h1 className='text-[#333] text-2xl'>{user?.companyName}</h1>
+                  </div>
+                  <div className='flex justify-between items-center'>
+                  <h1 className='text-[#333] text-2xl font-bold'>Phone : </h1>
+                  <h1 className='text-[#333] text-2xl'>{user?.phoneNumber}</h1>
+                  </div>
+                  <div className='flex justify-between items-center'>
+                  <h1 className='text-[#333] text-2xl font-bold'>Address : </h1>
+                  <h1 className='text-[#333] text-2xl'>{user?.address}</h1>
+                  </div>
+                  <div className='flex justify-between items-center'>
+                  <h1 className='text-[#333] text-2xl font-bold'>Joind Date :</h1>
+                  <h1 className='text-[#333] text-2xl'>{user?.joinDate?.substring(0, 10)}</h1>
+                  </div>
+                  <div className='flex justify-between items-center'>
+                  <h1 className='text-[#333] text-2xl font-bold'>Dealer licensed :</h1>
+                  <h1 className='text-[#333] text-2xl'>{user?.dealerlicensed}</h1>
+                  </div>
+              </div>
+        </div>
     </div>
     
 
