ef64728bd561065c77b01cde9474a639747618ab
eitanyacov <eitanyacov@gmail.com>
2022-06-23 05:41:57 +0300

    fixed chart

--- pages/scheduler/index.js
+++ pages/scheduler/index.js
@@ -25,9 +25,14 @@ import {
 
 const SchedulerPage = () => {
   const [open, setOpen] = useState(false);
+  const [id, setId] = useState()
   const [user, setUser] = useState({})
+  const [changed, setChanged] = useState([])
   const [date, setDate] = useState(new Date());
   const [schedulers, setSchedulers] = useState([])
+  
+  // const [startDate, setStartDate] = useState()
+  // const [endDate, setendDate] = useState()
   
   const router = useRouter();
   useEffect(()=> {
@@ -64,6 +69,8 @@ const SchedulerPage = () => {
   const dragDisableIds = new Set([3, 8, 10, 12]);
 
   const allowDrag = ({ id }) => !dragDisableIds.has(id);
+
+  
 
   const appointmentComponent = (props) => {
     if (allowDrag(props.data)) {
@@ -104,6 +111,20 @@ const SchedulerPage = () => {
     // setNotes(added?.notes)
     // console.log("title: " + title + "notes: " + notes)
     // console.log(title, startDate, endDate, notes)
+    // console.log("changed: " + changed)
+
+    // if(changed) {
+    //   setChanged(changed);
+    //   schedulers.map(s => {
+    //     if(changed[s.id]){
+    //       console.log(s.id)
+    //       setId(s.id)
+    //       // console.log(changed[id]?.startDate)
+    //       // console.log(changed[id]?.endDate)
+   
+         
+          
+ 
     console.log("deleted: ? " + deleted)
     if(deleted) {
       axios.delete("http://localhost:8080/api/user/delete-scheduler/" + deleted)
@@ -113,16 +134,18 @@ const SchedulerPage = () => {
        router.reload()
     }
 
+   if(added) {
     axios.post("http://localhost:8080/api/user/add-scheduler/" + user?.id, {
-            title: added?.title,
-            startDate: added?.startDate,
-            endDate: added?.endDate,
-            notes: added?.notes
-          }).then(res => {console.log(res.data)})
-          // .catch(error => setError(error.response.data))
-          .catch(error => console.log(error.response.data))
-           setOpen(true)
-           router.reload()
+      title: added?.title,
+      startDate: added?.startDate,
+      endDate: added?.endDate,
+      notes: added?.notes
+    }).then(res => {console.log(res.data)})
+    // .catch(error => setError(error.response.data))
+    .catch(error => console.log(error.response.data))
+     setOpen(true)
+     router.reload()
+   }
          
   }
   const currentDateChange = (currentDate) => {
